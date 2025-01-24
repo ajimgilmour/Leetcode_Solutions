0b38bad1e38181f5d695f04476f62d9e3cb9decf
ajimgilmour <jimscamper17@gmail.com>
2025-01-24 00:00:00 +0000

    Add solution for 2025-01-24_Find_Eventual_Safe_States_820.py solved on Fri, 24 Jan 2025 00:00:00 +0000

---
+++ Python/Medium/2025-01-24_Find_Eventual_Safe_States_820.py
@@ -0,0 +1,40 @@
+# Problem: Find Eventual Safe States
+# Question ID: 820
+# Difficulty: Medium
+# Solved Date: 2025-01-24
+# LeetCode URL: https://leetcode.com/problems/find-eventual-safe-states/
+
+// https://leetcode.com/problems/find-eventual-safe-states
+
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        n = len(graph)
+        indegree = [0] * n
+        adj = [[] for _ in range(n)]
+
+        for i in range(n):
+            for node in graph[i]:
+                adj[node].append(i)
+                indegree[i] += 1
+
+        q = deque()
+        for i in range(n):
+            if indegree[i] == 0:
+                q.append(i)
+
+        safe = [False] * n
+        while q:
+            node = q.popleft()
+            safe[node] = True
+
+            for neighbor in adj[node]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    q.append(neighbor)
+
+        safeNodes = []
+        for i in range(n):
+            if safe[i]:
+                safeNodes.append(i)
+
+        return safeNodes
